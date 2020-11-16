587a9c13d462a024e80112ad5e58f2a4bc16642b
Giovanni Salinas <gsalinaslopez@gmail.com>
2020-11-16 11:23:38 +0800

    foreground and background color changes

--- config.def.h
+++ config.def.h
@@ -121,6 +121,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	[256] = "#1d1f21", /* background */
 	[257] = "#c5c8c6", /* foreground */
+	[258] = "#000000",
+	[259] = "#dfdfdf",
 };
 
 
@@ -128,8 +130,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
 
